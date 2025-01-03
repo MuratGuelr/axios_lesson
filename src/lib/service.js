3932312c5d862dd5494813e55fc450ad6bc44a50
ConsolAktif <sigortataciri@gmail.com>
2025-01-03 13:33:47 +0300

    getting user and the first post function is created.

---
+++ src/lib/service.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const GetData = async (userId, getAll) => {
+  const { data: user } = await axios(
+    `https://jsonplaceholder.typicode.com/users/${userId}`
+  );
+
+  const { data: post } = await axios(
+    `https://jsonplaceholder.typicode.com/posts?userId=${userId}`
+  );
+
+  const onlyOnePost = post.length > 0 ? post[0] : null;
+
+  const data = {
+    user,
+    onlyOnePost,
+  };
+
+  return data;
+};
+
+export default GetData;
